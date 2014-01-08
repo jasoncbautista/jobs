df43fb07e6bee1574f742dad30d592ebc9d8f7a5
Jason B <bjason@gmail.com>
2014-01-08 04:48:23 -0800

    Fixed typos and bugs

---
+++ jobs.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+    var JobsApp = {};
+    JobsApp.createJobEl= function(job) {
+        return $("<div>" + job.description + "</div>");
+    };
+
+    JobsApp.renderList = function(listHolderEl, jobList){
+        _.each(jobList, function(job) {
+            console.log('job', job);
+            var jobEl = JobsApp.createJobEl(job);
+            listHolderEl.append(jobEl);
+        });
+    };
+
+    function reqListener () {
+        var jsonString = this.responseText;
+        console.log('string', jsonString);
+        var jsonObj = JSON.parse(jsonString);
+        console.log(jsonObj);
+        JobsApp.renderList($("#jobList"), jsonObj.jobs);
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open("get", "job_list.json", true);
+    oReq.send();
+});
